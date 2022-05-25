e4bff3f038289d09d736de7ae6b81e2553725019
Justin Rashid <91773223+Sh1roh@users.noreply.github.com>
2022-05-25 17:45:49 +0800

    Update finish.cs
    finish script changed

--- Emc10 lab 1/Assets/SCRIPTS/finish.cs
+++ Emc10 lab 1/Assets/SCRIPTS/finish.cs
@@ -22,6 +22,6 @@ public class finish : MonoBehaviour
 
     private void CompleteLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("End Screen");
     }
 }
